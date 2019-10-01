9f64020212db02332378dadbe662720112dc21c5
Henri J. Norden <ttu@norden.io>
2019-10-01 16:04:46 +0300

    Simplified data input, added table simplification functionality

--- solve_LP.py
+++ solve_LP.py
@@ -15,6 +15,40 @@ def _sub(l, lSub):
     for i in range(len(l)):
         l[i] -= lSub[i]
 
+def check_el_diag(l, log=False):
+    rows = len(l)-1
+    for c in range(-1, -rows-1, -1):
+        if l[0][c] != 0:
+            for r in range(1, rows+1):
+                if l[r][c] != 0:
+                    if log: print("Subtracting row {} from 0 at col {}".format(r, c))
+                    subtract = _div(l[r], Fraction(l[r][c], l[0][c]), True)
+                    _sub(l[0], subtract)
+                    break
+    if log:
+        print(getMatrix(l))
+
+def remove_lead_elements(l, log=False, removed=None):
+    rows = len(l)
+    cols = len(l[0])
+    if removed is None: removed = set()
+    newly_removed = set()
+    for c in range(1, cols):
+        nonzero = []
+        for r in range(1, rows):
+            if l[r][c] != 0:
+                nonzero.append((r,c))
+        if len(nonzero) == 1 and nonzero[0] not in removed:
+            r, c = nonzero[0]
+            if (l[0][c]) != 0:                
+                if log: print("Subtracting row {} from 0 at col {}".format(r, c))
+                subtract = _div(l[r], Fraction(l[r][c], l[0][c]), True)
+                _sub(l[0], subtract)
+                newly_removed.add(nonzero[0])
+    if log:
+        print(getMatrix(l))
+    return newly_removed
+
 def simplex_move(l, col, log=False):
     lead_row = max([(l[row][col] / l[row][0], row) for row in range(1, len(l))], key=lambda x: x[0])[1]
     if log: print("Minimal el and free val div on row: {}".format(lead_row))
@@ -53,46 +87,46 @@ def solve_simplex(l, log=False):
     
 
 def getInput():
-    size = [int(input("Simplex table rows: "))]
-    cols = input("Simplex table columns (leave empty for " + str(size[0]+1) + "): ")
-    if len(cols) == 0:
-        size.append(size[0] + 1)
-    else:
-        size.append(int(cols))
-
-    variables = input("Variable names: (leave empty for x1...x{}): ".format(size[1]-1)).split(" ")
-    if variables == ['']:
-        variables = ["x{}".format(i) for i in range(1, size[1]-1)]
-        
-    l = [[] for i in range(size[0])]
-    if size[0] < 2 or size[1] <= size[0]:
-        print("<rows> must be larger than 2 and <cols> must be larger <rows> + 1!")
-        return l
-    for i in range(size[0]):
-        while True:
-            l[i] = []
-            try:
-                vals = input("Row " + str(i) + ": ")
-                j = 0
-                for val in vals.split(" "):
-                    if "/" in val:
-                        val = list(map(int, val.split("/")))
-                        l[i].append(Fraction(*val))
-                    else:
-                        l[i].append(Fraction(float(val)))
-                    j += 1
-                if len(l[i]) != size[1]:
-                    print("Input does not match column count! (use spaces as separators between numbers)")
-                    continue
+    l = []
+    col_count = None
+    ex = int(input("Solver (1-simplex, 2-diag): "))
+    print("Input simplex table (separate elements with spaces, rows with new line; leave line empty to end input):")
+    while True:
+        row = []
+        try:
+            vals = input()
+            if vals == "":
                 break
-            except Exception as e:
-                if isinstance(e, KeyboardInterrupt):
-                    print("Input cancelled!\n")
-                    raise KeyboardInterrupt()
-                print("Input parsing failed!")
-                print(e)
+            for val in vals.split(" "):
+                if "/" in val:
+                    val = list(map(int, val.split("/")))
+                    row.append(Fraction(*val))
+                else:
+                    row.append(Fraction(float(val)))
+            if col_count is None:
+                col_count = len(row)
+            elif len(row) != col_count:
+                print("Input does not match column count! (use a single space as the separator between numbers)")
+                continue
+        except Exception as e:
+            if isinstance(e, KeyboardInterrupt):
+                print("Input cancelled!\n")
+                raise KeyboardInterrupt()
+            print("Input parsing failed!")
+            print(e)
+        l.append(row)
+
+    row_count = len(l)
+    if row_count < 2 or col_count <= row_count:
+        print("<rows> must be larger than 2 (got {}) and <cols> must be larger <rows> + 1 (got {})!".format(row_count, col_count))
+        return (0, [], [])
+
+    variables = input("Variable names: (leave empty for x1...x{}): ".format(col_count-1)).split(" ")
+    if variables == ['']:
+        variables = ["x{}".format(i) for i in range(1, col_count)]
     print()
-    return (variables, l)
+    
+    return (ex, variables, l)
 
 def _getStrings(l, getMax=False):
     lp = [[] for i in range(len(l))]
@@ -103,7 +137,7 @@ def _getStrings(l, getMax=False):
             maxLen = max(maxLen, len(lp[r][-1]))
     return lp if not getMax else (lp, maxLen)
 
-def getMatrix(l):
+def getMatrix(l, highlight=None):
     global var
     lp, maxLen = _getStrings(l, True)
     ls = []
@@ -157,14 +191,24 @@ def getValueStrings(l, var):
 
 l = [[]]
 var = []
+ex = 0
 while True:
     # calculate
     while len(l[0]) == 0:
         try:
-            var, l = getInput()
+            ex, var, l = getInput()
         except Exception as e:
             print("Input parsing failed!")
             print(e)
+            ex = 0
+    if ex == 0:
+        l = [[]]
+        continue
+    if ex == 2:
+        check_el_diag(l, True)
+    removed_els = set((-1, -1))
+    while len(removed_els) != 0:
+        removed_els = remove_lead_elements(l, True, removed_els)
     solved = solve_simplex(l, True)
     
     if solved: # print values
